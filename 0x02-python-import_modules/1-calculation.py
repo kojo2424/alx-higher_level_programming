aae43cd662922b5ef8262bcb95c797c799b3757d
kojo2424 <asmahcoffie2424@gmail.com>
2024-01-10 16:32:49 -0800

    python

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from calculator_1 import sub, mul, div
+a = 10
+b = 5
+
+if __name__ == "__main__":
+    result_1 = sub(a, b)
+    result_2 = mul(a, b)
+    result_3 = div(a, b)
+    print(result_1)
+    print(result_2)
+    print(result_3)
