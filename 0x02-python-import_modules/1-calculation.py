c91d746ba23a61df5403268d7123c280ccdae1b4
kojo2424 <asmahcoffie2424@gmail.com>
2024-01-10 16:41:08 -0800

    pytho

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-from calculator_1 import sub, mul, div
+from calculator_1 import sub, mul, div, add
 a = 10
 b = 5
-
 if __name__ == "__main__":
-    result_1 = sub(a, b)
-    result_2 = mul(a, b)
-    result_3 = div(a, b)
-    print(result_1)
-    print(result_2)
-    print(result_3)
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
